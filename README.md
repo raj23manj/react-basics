9c42f467f5a7eabe00807b4ec3aceefaf0e1e265
rajesh23-drg <rmanjunath@teamdrg.com>
2019-12-13 09:34:28 +0530

    Using Hooks

--- README.md
+++ README.md
@@ -19,13 +19,31 @@
     * props allow you to pass data from a parent (wrapping) component to a child (embedded) component.
 
   * state
+    * use this. to access as it is a class
     * Whilst props allow you to pass data down the component tree (and hence trigger an UI update), state is used to change the      component, well, state from within. Changes to state also trigger an UI update.
     * state is only available class based components that inherit from  Component from react 
     * state is use to make changes from within a component
     * if state changes it refreshes the dom like in ember(notify all) and angular() 
+    * this.setState({})
+      * used t0 manipulate the state, we need to pass what property of the object needs to change, it then merges(overrides) with    the state object. i'e it replaces the old state completetl
+
+  * react Hooks(16.8 +)
+    * can be used in normal function with out calss based definition to handle states just like class
+    * useState
+      * returns array with two elements
+        * 1st element => current state with changed array and one old array
+        * 2nd element => a function that updates the state, i'e react is aware of it and re-render the component
+      * when updating the state here (see App.js) it does not merge like class does, the other propertis in the object or hash
+        get left out. We need to manually add the other properties  
 
   * Difference between Props and State
     * Whenever state  changes (taught over the next lectures), the component will re-render and reflect the new state. The           difference to props  is, that this happens within one and the same component - you don't receive new data (props ) from        outside!  
+
+  * Supported Events By React
+    * https://reactjs.org/docs/events.html#supported-events
+    * https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/8124210#overview  
+
+  *   
 
 # Commands
   * new project 
--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,30 +1,89 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
-class App extends Component {
+// class App extends Component {
+//   // comes from component
+//   state = {
+//     persons: [
+//       { name: 'Max', age: 23 },
+//       { name: 'Manu', age: 24 },
+//     ]
+//   };
+
+//   switchNameHandler = () => {
+//     //console.log('was Clicked');
+//     // don't do this
+//     // this.state.persons[0] = "July";
+//     this.setState({
+//       persons: [
+//         { name: 'july', age: 23 },
+//         { name: 'Manu', age: 24 }
+//       ]
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hi yipee {50 * 40} </h1>
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
+//         {/* <Person name="Max" age="23" /> */}
+//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+//         <Person name="Bill" age="25">My Hobbies: Racing</Person>
+//         {/* <Person></Person> */}
+//       </div>
+//     );
+
+//     // return React.createElement('div', null, React.createElement('h1', null, 'Yipee React App !!!'));
+//     //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Yipee React App !!!'))
+//   }
+// }
+
+
+
+
+// React Hooks 
+
+const App = props => {
   // comes from component
-  state = {
-    persons: [
-      { name: 'Max', age: 23 },
-      { name: 'Manu', age: 24 },
-    ]
-  };
+  const [personState, setPersonState] = useState(
+    {
+      persons: [
+        { name: 'Max', age: 23 },
+        { name: 'Manu', age: 24 },
+      ],
+      otherState: 'some Other state'
+    }
+  );
+  // better way to manage other properties
+  const [otherState, setOtherState] = useState({ otherState: 'some Other state' });
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi yipee {50 * 40} </h1>
-        <button>Switch Name</button>
-        {/* <Person name="Max" age="23" /> */}
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name="Bill" age="25">My Hobbies: Racing</Person>
-        {/* <Person></Person> */}
-      </div>
-    );
+  console.log(personState, otherState);
 
-    // return React.createElement('div', null, React.createElement('h1', null, 'Yipee React App !!!'));
-    //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Yipee React App !!!'))
+  const switchNameHandler = () => {
+    //console.log('was Clicked');
+    // don't do this
+    // this.state.persons[0] = "July";
+    setPersonState({
+      persons: [
+        { name: 'july', age: 23 },
+        { name: 'Manu', age: 24 }
+      ]
+      // not a good way
+      //,otherState: personState.otherState // manually adding other states unlike in class based state
+    })
   }
-}
 
+  return (
+    <div className="App">
+      <h1>Hi yipee {50 * 40} </h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      {/* <Person name="Max" age="23" /> */}
+      <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+      <Person name="Bill" age="25">My Hobbies: Racing</Person>
+      {/* <Person></Person> */}
+    </div>
+  );
+
+}
 export default App;
