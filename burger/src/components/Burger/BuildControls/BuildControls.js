bdca53bfc2d84a4cc25cd2becbb9963245292dd9
rajesh23-drg <rmanjunath@teamdrg.com>
2020-01-05 08:21:38 +0530

    163-165

--- burger/src/components/Burger/BuildControls/BuildControls.js
+++ burger/src/components/Burger/BuildControls/BuildControls.js
@@ -11,9 +11,16 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {
             controls.map(ctrl => (  // anything returning jsx should be enclosed by () not {}
-                <BuildControl key={ctrl.label} label={ctrl.label} />
+                <BuildControl
+                    key={ctrl.label}
+                    label={ctrl.label}
+                    added={() => props.ingredientAdded(ctrl.type)}
+                    removed={() => props.ingredientRemoved(ctrl.type)}
+                    disabled={props.disabledInfo[ctrl.type]}
+                />
             ))
         }
     </div>
