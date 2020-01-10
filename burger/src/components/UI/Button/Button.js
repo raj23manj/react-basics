642f071dc7931b3623be1057a4201be8c288a40a
rajesh23-drg <rmanjunath@teamdrg.com>
2020-01-10 13:22:28 +0530

    Form Validation

--- burger/src/components/UI/Button/Button.js
+++ burger/src/components/UI/Button/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
+
 import classes from './Button.css';
 
-// dynamic css assigning
 const button = (props) => (
     <button
+        disabled={props.disabled}
         className={[classes.Button, classes[props.btnType]].join(' ')}
-        onClick={props.clicked}
-    >{props.children}</button>
+        onClick={props.clicked}>{props.children}</button>
 );
 
 export default button;
