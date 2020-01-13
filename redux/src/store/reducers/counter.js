d1ff7becd26d3d17980b4886a9ff2c4d1df4ea8b
rajesh23-drg <rmanjunath@teamdrg.com>
2020-01-13 11:35:33 +0530

    splitting up reducers

---
+++ redux/src/store/reducers/counter.js
@@ -0,0 +1,36 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    counter: 0
+};
+
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case actionTypes.INCREMENT:
+            // copy old object 
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
+        case actionTypes.DECREMENT:
+            // both above method and this method is fine
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case actionTypes.ADD:
+            return {
+                ...state,
+                counter: state.counter + action.val
+            }
+        case actionTypes.SUBTRACT:
+            return {
+                ...state,
+                counter: state.counter - action.val
+            }
+    }
+
+    return state;
+}
+
+export default reducer;
---
+++ redux/src/store/reducers/result.js
@@ -0,0 +1,39 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    results: []
+};
+
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                // array push manipulates the old array, hence concat returns a new array
+                // immutable way of updating an array by adding an item 293
+                // both the redcures will be merged as one hence we can get state.counter, but can access directly
+                // hence pass it as action
+                results: state.results.concat({ id: new Date(), value: action.result })
+            }
+        case actionTypes.DELETE_RESULT:
+            // this mutates not good
+            //state.results.splice(id, 1)
+            // in this case if induvudualt elements are objects, they will still point to old objects, only if deleting
+            // this can be used, as we are removing the referrence of the objects
+            // const newArray = [...state.results];
+            // newArray.results.splice(id, 1)
+            // another way and common, and gives a new array, if we return true return for every element with new array
+            // const updatedArray = state.results.filter(result => true);
+            const updatedArray = state.results.filter((result) => result.id !== action.resultElId);
+
+            return {
+                ...state,
+                results: updatedArray
+            }
+    }
+
+    return state;
+}
+
+export default reducer;
